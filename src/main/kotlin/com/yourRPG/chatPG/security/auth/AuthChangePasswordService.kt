4a4322ff529ef99a615af5d8d5cd1eaec86198df
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 12:16:51 -0300

    Changed AuthChangePasswordService.kt to make use of the reworked Request Handle.

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthChangePasswordService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthChangePasswordService.kt
@@ -1,33 +1,45 @@
 package com.yourRPG.chatPG.security.auth
 
 import com.yourRPG.chatPG.domain.account.Account
-import com.yourRPG.chatPG.dto.auth.account.ChangePasswordDto
-import com.yourRPG.chatPG.exception.auth.password.PasswordResetException
+import com.yourRPG.chatPG.dto.auth.account.ConfirmChangePasswordDto
+import com.yourRPG.chatPG.infra.email.EmailService
 import com.yourRPG.chatPG.infra.email.MimeHelper
 import com.yourRPG.chatPG.infra.uri.FrontendUriHelper
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleService
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleSubject
 import com.yourRPG.chatPG.service.account.AccountService
-import com.yourRPG.chatPG.infra.email.EmailService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.Duration
-import java.time.Instant
-import java.util.UUID
+import java.util.*
 
 @Service
 class AuthChangePasswordService(
+    private val passwordEncoder: PasswordEncoder,
     private val accountService: AccountService,
-    private val mimeHelper: MimeHelper,
-    private val frontendUriHelper: FrontendUriHelper,
     private val emailService: EmailService,
-    private val passwordService: AuthPasswordService
+    private val requestHandleService: RequestHandleService,
+
+    private val frontendUriHelper: FrontendUriHelper,
+    private val mimeHelper: MimeHelper,
+
+    @param:Value("\${security.request-handle.change-password-expires-in}")
+    private val changePasswordExpiresIn: Duration,
+
 ) {
 
+    companion object {
+        private val subject = RequestHandleSubject.CHANGE_PASSWORD
+    }
+
+    @Transactional
     fun requestChangePassword(email: String) {
         val account: Account =
             accountService.getByEmail(email)
 
-        val uuid = UUID.randomUUID()
-
-        accountService.updateUuid(account, uuid)
+        val uuid = requestHandleService.newRequestHandle(account, subject)
 
         val url = frontendUriHelper.appendString("/login/forgot-password/$uuid")
         val html = mimeHelper.getTemplate(
@@ -43,28 +55,18 @@ class AuthChangePasswordService(
 
     }
 
-    fun confirmChangePassword(changePassword: ChangePasswordDto): Unit = changePassword.run {
-        val uuid = UUID.fromString(changePassword.uuid)
+    @Transactional
+    fun confirmChangePassword(dto: ConfirmChangePasswordDto): Unit = dto.run {
+        val uuidV7 = UUID.fromString(dto.requestHandle)
 
         val account: Account =
-            accountService.getByUuid(uuid)
+            requestHandleService.getAccountAndDiscardCheckedHandle(uuidV7,
+                subject,
+                changePasswordExpiresIn
+            )
 
-        accountService.updateUuid(account, null)
-
-        val birth = account.auth.uuidBirth
-            ?: throw PasswordResetException("Uuid does not have instant of creation")
-
-        val fifteenMinutes = Duration.ofMinutes(15)
-        val fifteenMinutesAgo = Instant.now().minus(fifteenMinutes)
-
-        val stillValid = birth.isAfter(fifteenMinutesAgo)
-
-        if (stillValid) {
-            val encryptedPassword = passwordService.encrypt(rawPassword = changePassword.password)
-
-            accountService.updatePassword(account, encryptedPassword)
-        }
-
+        val encodedPassword = passwordEncoder.encode(dto.password)
+        accountService.updatePassword(account, encodedPassword)
     }
 
 }
