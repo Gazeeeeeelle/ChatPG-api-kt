79d0eab971c793ee454e0d188289f663a6aab084
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 12:45:13 -0300

    Removed unused @Value.

--- src/main/kotlin/com/yourRPG/chatPG/infra/email/MimeHelper.kt
+++ src/main/kotlin/com/yourRPG/chatPG/infra/email/MimeHelper.kt
@@ -1,17 +1,13 @@
 package com.yourRPG.chatPG.infra.email
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
 
 @Component
 class MimeHelper(
-    private val templateEngine: TemplateEngine
+    private val templateEngine: TemplateEngine,
 ) {
-
-    @Value("\${server.frontend.address}")
-    private lateinit var frontEndAddress: String
 
     fun getTemplate(template: String, vararg variables: Pair<String, String>): String {
         val context = Context().apply {
