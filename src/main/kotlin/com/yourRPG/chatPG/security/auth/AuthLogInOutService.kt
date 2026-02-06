7634ae936e60236d2a1dbc5849210a050e6622d1
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 12:21:13 -0300

    Improved /login endpoint's security by reducing efficiency of timing attacks. Added Exception Driven Outcome in case the account has Two-Factor Authentication enabled.

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthLogInOutService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthLogInOutService.kt
@@ -1,36 +1,53 @@
 package com.yourRPG.chatPG.security.auth
 
+import com.yourRPG.chatPG.domain.account.Account
 import com.yourRPG.chatPG.dto.auth.LoginCredentials
 import com.yourRPG.chatPG.dto.auth.TokenDto
-import com.yourRPG.chatPG.exception.account.AccessToAccountUnauthorizedException
+import com.yourRPG.chatPG.exception.http.NotFoundException
+import com.yourRPG.chatPG.exception.http.UnauthorizedException
 import com.yourRPG.chatPG.security.token.TokenManagerService
 import com.yourRPG.chatPG.service.account.AccountService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class AuthLogInOutService(
-    private val authPasswordService: AuthPasswordService,
+    private val passwordEncoder: PasswordEncoder,
     private val tokenManagerService: TokenManagerService,
     private val accountService: AccountService,
+    private val authA2FService: AuthA2FService,
 ) {
 
+    companion object {
+        private const val DUMMY_ENCODED_PASSWORD = "$2a$10$58r.Gqy1ncdfGOkov08MguLtb/G18l5wcE2BTze1wx4JPUHGYtfFC"
+    }
+
+    //All outcomes that change the DB's state happen at the end of the method.
     fun login(credentials: LoginCredentials): Pair<TokenDto, String> {
-        val account = accountService.getByName(credentials.username)
+        val account: Account? = try {
+            accountService.getByName(credentials.username)
+        } catch (_: NotFoundException) { //Masks status for security.
+            null
+        }
 
-        authPasswordService.passwordEncoder.matches(credentials.password, account.password)
-            .takeIf { it }
-            ?: throw AccessToAccountUnauthorizedException("Wrong password")
+        //Performs the PasswordEncoder.matches not to give away discrepancies between non-existent account and wrong
+        // password
+        val passwordMatches = passwordEncoder.matches(
+            credentials.password,
+            account?.password ?: DUMMY_ENCODED_PASSWORD
+        )
 
-        val token = tokenManagerService.signAccessToken(account)
+        if (!passwordMatches || account == null) {
+            throw UnauthorizedException("Invalid credentials")
+        }
 
-        val refreshToken = tokenManagerService.signRefreshToken(account)
-
-        return TokenDto(token) to refreshToken
+        return if (account.auth.a2f) authA2FService.requireA2F(account)
+            else tokenManagerService.requireRefreshToken(account)
     }
 
     fun logout(accountId: Long) =
         accountService.getById(accountId).let { account ->
-            accountService.saveWithRefreshToken(account, refreshToken = null)
+            accountService.updateRefreshToken(account, refreshToken = null)
         }
 
 }
