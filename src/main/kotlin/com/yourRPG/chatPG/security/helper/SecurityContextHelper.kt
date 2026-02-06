fe4e9443f6c1a23a82fc6328024f26925ba65dac
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 12:49:01 -0300

    Removed unused import.

--- src/main/kotlin/com/yourRPG/chatPG/security/helper/SecurityContextHelper.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/helper/SecurityContextHelper.kt
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class SecurityContextHelper {
