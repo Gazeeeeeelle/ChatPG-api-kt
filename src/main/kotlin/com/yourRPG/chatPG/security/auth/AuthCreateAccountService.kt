ffa82ef8837c8d4c72407e08a6f47460f43976fb
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 12:18:21 -0300

    Changed AuthChangePasswordService.kt to make use of the reworked Request Handle.

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthCreateAccountService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthCreateAccountService.kt
@@ -4,58 +4,70 @@ import com.yourRPG.chatPG.domain.account.Account
 import com.yourRPG.chatPG.dto.account.AccountDto
 import com.yourRPG.chatPG.dto.auth.UuidDto
 import com.yourRPG.chatPG.dto.auth.account.CreateAccountDto
-import com.yourRPG.chatPG.exception.http.ConflictException
 import com.yourRPG.chatPG.exception.auth.AccountActivationException
+import com.yourRPG.chatPG.exception.http.ConflictException
+import com.yourRPG.chatPG.infra.email.EmailService
 import com.yourRPG.chatPG.infra.email.MimeHelper
 import com.yourRPG.chatPG.infra.uri.FrontendUriHelper
+import com.yourRPG.chatPG.infra.uuid.UuidHelper
+import com.yourRPG.chatPG.mapper.AccountMapper
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleService
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleSubject
 import com.yourRPG.chatPG.service.account.AccountService
 import com.yourRPG.chatPG.service.account.AccountStatus
-import com.yourRPG.chatPG.infra.email.EmailService
-import com.yourRPG.chatPG.mapper.AccountMapper
 import com.yourRPG.chatPG.validator.account.PasswordValidator
 import com.yourRPG.chatPG.validator.account.UsernameValidator
 import jakarta.transaction.Transactional
-import org.springframework.data.jpa.repository.Modifying
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
-import java.time.Instant
-import java.time.temporal.ChronoUnit
+import java.time.Duration
 import java.util.*
 
 @Service
 class AuthCreateAccountService(
-    private val passwordService: AuthPasswordService,
+    private val passwordEncoder: PasswordEncoder,
     private val accountService: AccountService,
     private val emailService: EmailService,
+    private val requestHandleService: RequestHandleService,
 
     private val usernameValidator: UsernameValidator,
     private val passwordValidator: PasswordValidator,
 
     private val mimeHelper: MimeHelper,
     private val frontendUriHelper: FrontendUriHelper,
+    private val uuidHelper: UuidHelper,
 
     private val accountMapper: AccountMapper,
+
+    @param:Value("\${security.request-handle.activate-account-expires-in}")
+    private val activateAccountExpiresIn: Duration,
+
+    @param:Value("\${frontend.paths.activate-account}")
+    private val activateAccountUrl: String,
 ) {
 
-    @Modifying
-    @Transactional
-    fun createAccount(dto: CreateAccountDto): AccountDto {
+    companion object {
+        private val subject = RequestHandleSubject.ACTIVATE_ACCOUNT
+    }
 
+    @Transactional
+    fun openAccountCreation(dto: CreateAccountDto): AccountDto {
         usernameValidator.validate(t = dto.username)
         passwordValidator.validate(t = dto.password)
 
-        val encryptedPassword = passwordService.encrypt(rawPassword = dto.password)
+        val encryptedPassword = passwordEncoder.encode(dto.password)
 
-        val account = accountService.saveAccountWith(dto.username, dto.email, encryptedPassword)
+        val account = accountService.insertAccount(dto.username, dto.email, encryptedPassword)
 
-        val uuid = UUID.randomUUID()
-
-        accountService.updateUuid(account, uuid)
+        val handle = requestHandleService.newRequestHandle(
+            account,
+            subject
+        )
 
         val html = mimeHelper.getTemplate(
-            template  = "mime-activate-account",
-            variables = arrayOf(
-                "url" to frontendUriHelper.appendString("/login/activate-account/$uuid")
-            )
+            template = "mime-activate-account",
+            "url" to frontendUriHelper.appendString("$activateAccountUrl?uuid=$handle")
         )
 
         emailService.sendMimeEmail(
@@ -67,33 +79,31 @@ class AuthCreateAccountService(
         return accountMapper.toDto(account)
     }
 
-    @Modifying
-    @Transactional
-    fun activateAccount(dto: UuidDto): AccountDto {
-        val account: Account = accountService.getByUuid(UUID.fromString(dto.uuid))
+    @Transactional(dontRollbackOn = [AccountActivationException::class])
+    fun fulfillAccountCreation(dto: UuidDto): AccountDto {
+        val requestHandle = UUID.fromString(dto.uuid)
+        val account: Account =
+            requestHandleService.getAccountByCheckedRequestHandle(
+                requestHandle,
+                subject,
+                activateAccountExpiresIn
+            )
 
-        accountService.updateUuid(account, null)
-
-        when (account.auth.status) {
+        when (account.status) {
             AccountStatus.DISABLED -> {}
             AccountStatus.ENABLED  -> throw ConflictException("Account already activated")
             AccountStatus.DELETED  -> throw ConflictException("This account is deleted")
         }
 
-        val birth = account.auth.uuidBirth
-            ?: throw AccountActivationException("Uuid does not have instant of creation")
+        accountService.removeHandle(account)
 
-        val thirtyMinutesAgo = Instant.now()
-            .minus(30, ChronoUnit.MINUTES)
-
-        if (birth.isAfter(thirtyMinutesAgo)) {
+        if (uuidHelper.isNotExpired(uuidV7 = requestHandle, expirationTime = activateAccountExpiresIn)) {
             accountService.updateStatus(account, status = AccountStatus.ENABLED)
             return accountMapper.toDto(account)
         }
 
         accountService.deleteById(account.id)
-
-        throw AccountActivationException("Request expired. Request will be deleted")
+        throw AccountActivationException("Request expired. Request will be deleted. ")
     }
 
 }
