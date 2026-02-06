613126da0d7a5be25a9fff07073ef9be5706a9f0
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 13:30:31 -0300

    Removed validator constraint to enable more versatility in malformed uuid's outcome.

--- src/main/kotlin/com/yourRPG/chatPG/dto/auth/UuidDto.kt
+++ src/main/kotlin/com/yourRPG/chatPG/dto/auth/UuidDto.kt
@@ -1,10 +1,7 @@
 package com.yourRPG.chatPG.dto.auth
 
-import org.hibernate.validator.constraints.UUID
-
 data class UuidDto(
 
-    @field:UUID(message = "Invalid link")
     val uuid: String
 
 )
