442d55968000865778e2272b3096a94928003cc0
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-06 14:13:39 -0300

    Fixed endpoints. The sub paths were already prefixed with the controller's and did not require to interpolate with their respective base.

--- src/main/kotlin/com/yourRPG/chatPG/config/ApplicationEndpoints.kt
+++ src/main/kotlin/com/yourRPG/chatPG/config/ApplicationEndpoints.kt
@@ -5,43 +5,43 @@ object ApplicationEndpoints {
     object Auth {
         const val BASE = "/auth"
 
-        const val                    LOGIN = "$BASE/login"
-        const val           REFRESH_TOKENS = "$BASE/refreshTokens"
-        const val     OPEN_PASSWORD_CHANGE = "$BASE/open-password-change"
-        const val  FULFILL_PASSWORD_CHANGE = "$BASE/fulfill-password-change"
-        const val    OPEN_ACCOUNT_CREATION = "$BASE/open-account-creation"
-        const val FULFILL_ACCOUNT_CREATION = "$BASE/fulfill-account-creation"
+        const val                    LOGIN = "/login"
+        const val           REFRESH_TOKENS = "/refreshTokens"
+        const val     OPEN_PASSWORD_CHANGE = "/open-password-change"
+        const val  FULFILL_PASSWORD_CHANGE = "/fulfill-password-change"
+        const val    OPEN_ACCOUNT_CREATION = "/open-account-creation"
+        const val FULFILL_ACCOUNT_CREATION = "/fulfill-account-creation"
     }
 
     object AuthSecure {
         const val BASE = "${Auth.BASE}/secure"
 
-        const val                LOGOUT = "$BASE/logout"
-        const val REQUIRE_REFRESH_TOKEN = "$BASE/require-refresh-tokens"
+        const val                LOGOUT = "/logout"
+        const val REQUIRE_REFRESH_TOKEN = "/require-refresh-tokens"
     }
 
     object Account {
         const val BASE = "/account"
 
-        const val CURRENT = "$BASE/current"
+        const val CURRENT = "/current"
     }
 
     object AiModel {
         const val BASE = "/ai-models"
 
-        const val                ALL = "$BASE/all"
-        const val IS_MODEL_AVAILABLE = "$BASE/is-model-available"
+        const val                ALL = "/all"
+        const val IS_MODEL_AVAILABLE = "/is-model-available"
     }
 
     object Chat {
         const val BASE = "/chats"
 
-        const val          ALL = "$BASE/all"
+        const val          ALL = "/all"
         /**Path Variables: chatName*/
-        const val      BY_NAME = "$BASE/by-name/{chatName}"
-        const val CHOOSE_MODEL = "$BASE/choose-model"
+        const val      BY_NAME = "/by-name/{chatName}"
+        const val CHOOSE_MODEL = "/choose-model"
         /**Path Variables: chatId*/
-        const val        MODEL = "$BASE/{chatId}/model"
+        const val        MODEL = "/{chatId}/model"
     }
 
     object Message {
@@ -49,23 +49,23 @@ object ApplicationEndpoints {
         const val BASE = "${Chat.BASE}/{chatId}/messages"
 
         /**Path Variables: referenceId*/
-        const val               NEW = "$BASE/new/{referenceId}"
+        const val               NEW = "/new/{referenceId}"
         /**Path Variables: referenceId*/
-        const val               OLD = "$BASE/old/{referenceId}"
+        const val               OLD = "/old/{referenceId}"
         /**Path Variables: messageId*/
-        const val       GET_MESSAGE = "$BASE/{messageId}"
-        const val GENERATE_RESPONSE = "$BASE/generate-response"
-        const val            DELETE = "$BASE/{messageId}"
+        const val       GET_MESSAGE = "/{messageId}"
+        const val GENERATE_RESPONSE = "/generate-response"
+        const val            DELETE = "/{messageId}"
         /**Path Variables: bound1, bound2*/
-        const val       BULK_DELETE = "$BASE/bulk-delete/{bound1}/{bound2}"
+        const val       BULK_DELETE = "/bulk-delete/{bound1}/{bound2}"
     }
 
     object Poll {
         const val BASE = "${Chat.BASE}/polls"
 
-        const val START = "$BASE/start"
-        const val  VOTE = "$BASE/vote"
-        const val   ALL = "$BASE/all"
+        const val START = "/start"
+        const val  VOTE = "/vote"
+        const val   ALL = "/all"
     }
 
 }
