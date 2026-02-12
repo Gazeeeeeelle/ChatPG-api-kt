be61bec56530fc3360b18f4b86a3e361296ab9cc
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 11:12:24 -0300

    Removed redundant "Request" in the Dto's name.

--- src/main/kotlin/com/yourRPG/chatPG/dto/auth/FulfillA2fDto.kt
+++ src/main/kotlin/com/yourRPG/chatPG/dto/auth/FulfillA2fDto.kt
@@ -3,7 +3,7 @@ package com.yourRPG.chatPG.dto.auth
 import jakarta.validation.constraints.NotBlank
 import java.util.UUID
 
-data class FulfillA2FRequestDto(
+data class FulfillA2fDto(
 
     val requestHandle: UUID,
 
