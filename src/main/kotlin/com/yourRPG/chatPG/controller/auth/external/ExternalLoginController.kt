e8e775ef21e74f73230f9c7ab22812ff107a4ed5
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 11:16:43 -0300

    Replaced the use of Void as ResponseEntity's response type to Unit, which is more idiomatic for Kotlin.

--- src/main/kotlin/com/yourRPG/chatPG/controller/auth/external/ExternalLoginController.kt
+++ src/main/kotlin/com/yourRPG/chatPG/controller/auth/external/ExternalLoginController.kt
@@ -11,11 +11,11 @@ import java.net.URI
 
 open class ExternalLoginController(
     private val externalLoginService: ExternalLoginService,
-    private val frontendUriHelper: FrontendUriHelper
+    private val frontendUriHelper: FrontendUriHelper,
 ) {
 
     @GetMapping
-    fun codeUrl(): ResponseEntity<Void> {
+    fun codeUrl(): ResponseEntity<Unit> {
         val url = externalLoginService.getCodeUrl()
 
         val headers = HttpHeaders().apply { location = URI.create(url) }
@@ -27,11 +27,11 @@ open class ExternalLoginController(
     @GetMapping("/authorized")
     fun authorized(
         @RequestParam code: String
-    ): ResponseEntity<Void> {
-        val accessToken = externalLoginService.loginWithCode(code)
+    ): ResponseEntity<Unit> {
+        val requestHandle = externalLoginService.loginWithCode(code)
 
         val headers = HttpHeaders().apply {
-            location = frontendUriHelper.run { appendUri("$authorizedUrl?token=$accessToken") }
+            location = frontendUriHelper.run { appendUri("$authorizedPath?uuid=$requestHandle") }
         }
 
         return ResponseEntity.status(HttpStatus.FOUND)
