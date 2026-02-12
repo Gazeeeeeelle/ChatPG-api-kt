72dcdc580daf162ec4a1a3cd586d49c2e8543afb
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 11:16:05 -0300

    Updated GlobalExceptionHandler.kt to make use of RedirectDto.kt. Replaced ResponseEntity.status(400) to ResponseEntity.badRequest() for readability. Added handling for MissingRequestCookieException exception.

--- src/main/kotlin/com/yourRPG/chatPG/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/yourRPG/chatPG/exception/handler/GlobalExceptionHandler.kt
@@ -1,33 +1,34 @@
 package com.yourRPG.chatPG.exception.handler
 
-import com.yourRPG.chatPG.exception.auth.A2FRequiredException
+import com.yourRPG.chatPG.dto.error.RedirectDto
+import com.yourRPG.chatPG.exception.auth.A2fRequiredException
 import com.yourRPG.chatPG.exception.http.HttpException
-import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingRequestCookieException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(A2FRequiredException::class)
-    fun a2FRequiredException(ex: A2FRequiredException): ResponseEntity<Unit> {
-        val headers = HttpHeaders().apply {
-            location = ex.uri
-        }
-
-        return ResponseEntity.status(302)
-            .headers(headers)
-            .build()
+    @ExceptionHandler(A2fRequiredException::class)
+    fun a2fRequiredException(ex: A2fRequiredException): ResponseEntity<RedirectDto> {
+        val dto = RedirectDto("Further authentication needed.", ex.url)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(dto)
     }
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun methodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<String> =
-        ResponseEntity.status(400).body(ex.fieldError?.defaultMessage)
 
     @ExceptionHandler(HttpException::class)
     fun httpException(ex: HttpException): ResponseEntity<String> =
         ResponseEntity.status(ex.status).body(ex.message)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<String> =
+        ResponseEntity.badRequest().body(ex.fieldError?.defaultMessage)
+
+    @ExceptionHandler(MissingRequestCookieException::class)
+    fun missingRequestCookieException(ex: MissingRequestCookieException): ResponseEntity<String> =
+        ResponseEntity.badRequest().body("${ex.cookieName} is missing.")
 
 }
