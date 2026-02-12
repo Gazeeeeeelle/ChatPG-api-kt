2941c588529f9a8fec5f6d8914204c580e64fe5c
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 17:31:42 -0300

    Added ExpiredRequestHandleException.kt with field accountId, which isn't in its toString nor its JSON representation to keep the sensitive information from being logged out. The accountId can be used to reference the account found, whose Request Handle has expired.

---
+++ src/main/kotlin/com/yourRPG/chatPG/exception/requesthandle/ExpiredRequestHandleException.kt
@@ -0,0 +1,15 @@
+package com.yourRPG.chatPG.exception.requesthandle
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+class ExpiredRequestHandleException(
+    message: String? = null,
+    @field:JsonIgnore
+    val accountId: Long
+): IllegalStateException(message) {
+
+    override fun toString(): String {
+        return "ExpiredRequestHandleException: $message"
+    }
+
+}
