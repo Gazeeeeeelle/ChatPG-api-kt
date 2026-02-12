5ce6719277dc558e04d510ca48fcc901f69b449f
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 11:35:51 -0300

    Marked internals as such. abstracted interactions with EmailService and MimeHelper its own method. Fixed the missing usage of code for fulfillA2f. Renamed openA2FRequest to openA2f, since "Request" was redundant. Now Two-Factor Authentication is referred to in camelCase as "a2f" or "...A2f".

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthA2fService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthA2fService.kt
@@ -1,23 +1,23 @@
 package com.yourRPG.chatPG.security.auth
 
 import com.yourRPG.chatPG.domain.account.Account
-import com.yourRPG.chatPG.dto.auth.FulfillA2FRequestDto
-import com.yourRPG.chatPG.exception.auth.A2FRequiredException
+import com.yourRPG.chatPG.dto.auth.FulfillA2fDto
+import com.yourRPG.chatPG.exception.auth.A2fRequiredException
 import com.yourRPG.chatPG.infra.email.EmailService
 import com.yourRPG.chatPG.infra.email.MimeHelper
 import com.yourRPG.chatPG.infra.uri.FrontendUriHelper
 import com.yourRPG.chatPG.security.requesthandle.RequestHandleService
 import com.yourRPG.chatPG.security.requesthandle.RequestHandleSubject
+import com.yourRPG.chatPG.security.token.AccessAndRefreshTokens
 import com.yourRPG.chatPG.security.token.TokenManagerService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.net.URI
 import java.time.Duration
 import java.util.*
 
 @Service
-class AuthA2FService(
+class AuthA2fService(
     private val requestHandleService: RequestHandleService,
     private val emailService: EmailService,
     private val tokenManagerService: TokenManagerService,
@@ -33,54 +33,53 @@ class AuthA2FService(
         private val subject = RequestHandleSubject.A2F
     }
 
-    fun getUrlString(handle: UUID): String =
+    internal fun buildUrl(handle: UUID): String =
         frontendUriHelper.run {
-            appendString("$a2fUrl?uuid=$handle")
+            appendString("$a2fPath?uuid=$handle")
         }
 
-    fun getUrl(handle: UUID): URI = URI.create(getUrlString(handle))
-
     @Transactional
-    fun openA2FRequest(account: Account): UUID {
+    internal fun openA2f(account: Account): UUID {
         val email = requireNotNull(account.auth.credentials.email) { "Null email" }
 
-        val (handle, code) = requestHandleService.newRequestHandleWithCode(
-            account,
-            subject
-        )
+        val (handle, code) = requestHandleService.newRequestHandleWithCode(account, subject)
 
-        val html = mimeHelper.getTemplate(
-            template  = "mime-a2f",
-            "code" to code
-        )
-
-        emailService.sendMimeEmail(
-            subject = "Two-factor Authentication",
-            to = email,
-            html
-        )
+        sendA2FEmail(to = email, code)
 
         return handle
     }
 
-    @Transactional
-    fun fulfillA2FRequest(dto: FulfillA2FRequestDto) {
-        val account = requestHandleService.getAccountAndDiscardCheckedHandle(
-            uuid = dto.requestHandle,
-            subject,
-            expirationTime = a2FRequestExpiresIn
+    internal fun sendA2FEmail(to: String, code: String) {
+        emailService.sendMimeEmail(
+            subject = "Two-factor Authentication",
+            to,
+            html =
+                mimeHelper.getTemplate(
+                    template  = "mime-a2f",
+                    "code" to code
+                )
         )
+    }
 
-        tokenManagerService.signRefreshToken(account)
+    @Transactional
+    fun fulfillA2f(dto: FulfillA2fDto): AccessAndRefreshTokens {
+        val account =
+            requestHandleService.getAccountAndDiscardCheckedHandle(
+                uuid = dto.requestHandle,
+                subject,
+                code = dto.code,
+                expirationTime = a2FRequestExpiresIn
+            )
 
+        return tokenManagerService.signAccessAndRefreshTokens(account)
     }
 
     //Return type Nothing could not be used because it generated problem in tests.
     fun requireA2F(account: Account) {
-        val handle = openA2FRequest(account)
-        val url = getUrl(handle)
+        val handle = openA2f(account)
+        val url = buildUrl(handle)
 
-        throw A2FRequiredException(url)
+        throw A2fRequiredException(url)
     }
 
 }
