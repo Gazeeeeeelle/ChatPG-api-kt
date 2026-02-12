d213eb003ecea238f031d301551cee59d1f29fea
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 17:24:20 -0300

    Renamed encryptedPassword to encodedPassword to keep consistent with the rest of the application. Refactored Account status checking to isAccountDisabledElseThrow. Used try-catch on ExpiredRequestHandleException to determine if the request handle was expired, and then using the exception's field accountId to reference the account for deletion. Added logs still to be refined.

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthLogInOutService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthLogInOutService.kt
@@ -2,33 +2,47 @@ package com.yourRPG.chatPG.security.auth
 
 import com.yourRPG.chatPG.domain.account.Account
 import com.yourRPG.chatPG.dto.auth.LoginCredentials
-import com.yourRPG.chatPG.dto.auth.TokenDto
+import com.yourRPG.chatPG.dto.auth.UuidDto
 import com.yourRPG.chatPG.exception.http.NotFoundException
 import com.yourRPG.chatPG.exception.http.UnauthorizedException
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleService
+import com.yourRPG.chatPG.security.requesthandle.RequestHandleSubject
+import com.yourRPG.chatPG.security.token.AccessAndRefreshTokens
 import com.yourRPG.chatPG.security.token.TokenManagerService
 import com.yourRPG.chatPG.service.account.AccountService
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class AuthLogInOutService(
     private val passwordEncoder: PasswordEncoder,
     private val tokenManagerService: TokenManagerService,
     private val accountService: AccountService,
-    private val authA2FService: AuthA2FService,
+    private val authA2FService: AuthA2fService,
+    private val requestHandleService: RequestHandleService,
+
+    @param:Value("\${security.request-handle.login-with-handle-expires-in}")
+    private val loginWithHandleExpiresIn: Duration
 ) {
 
     companion object {
+        private val log = KotlinLogging.logger {}
+
         private const val DUMMY_ENCODED_PASSWORD = "$2a$10$58r.Gqy1ncdfGOkov08MguLtb/G18l5wcE2BTze1wx4JPUHGYtfFC"
     }
 
     //All outcomes that change the DB's state happen at the end of the method.
-    fun login(credentials: LoginCredentials): Pair<TokenDto, String> {
-        val account: Account? = try {
-            accountService.getByName(credentials.username)
-        } catch (_: NotFoundException) { //Masks status for security.
-            null
-        }
+    fun login(credentials: LoginCredentials): AccessAndRefreshTokens {
+        val account: Account? =
+            try {
+                accountService.getByName(credentials.username)
+            } catch (ex: NotFoundException) { //Masks status for security.
+                log.info { "Account not found for login" }
+                null
+            }
 
         //Performs the PasswordEncoder.matches not to give away discrepancies between non-existent account and wrong
         // password
@@ -38,16 +52,32 @@ class AuthLogInOutService(
         )
 
         if (!passwordMatches || account == null) {
+            if (!passwordMatches) log.info { "Wrong password" } //FIXME
             throw UnauthorizedException("Invalid credentials")
         }
 
-        if (account.auth.a2f) authA2FService.requireA2F(account) //Always throws A2FRequiredException
-        return tokenManagerService.requireRefreshToken(account)
+        if (account.auth.a2f) {
+            authA2FService.requireA2F(account)//Always throws A2FRequiredException
+        }
+
+        val tokens = tokenManagerService.signAccessAndRefreshTokens(account)
+        log.info { "Successful login" }
+        return tokens
     }
 
     fun logout(accountId: Long) =
         accountService.getById(accountId).let { account ->
             accountService.updateRefreshToken(account, refreshToken = null)
         }
+
+    fun loginWithHandle(uuidDto: UuidDto): AccessAndRefreshTokens {
+        val account = requestHandleService.getAccountAndDiscardCheckedHandle(
+            uuidDto.uuid,
+            RequestHandleSubject.EXTERNAL_LOGIN,
+            loginWithHandleExpiresIn
+        )
+
+        return tokenManagerService.signAccessAndRefreshTokens(account)
+    }
 
 }
