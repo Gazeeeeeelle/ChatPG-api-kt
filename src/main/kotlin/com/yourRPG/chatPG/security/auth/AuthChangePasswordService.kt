d1b33f1b2d6f6758c87203fdadaa00fa5f9605f7
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-12 11:43:33 -0300

    In fulfillPasswordChange, removed unhelpful usage of Kotlin's 'run' and fixed indentation pattern for one argument per line in usage of getAccountAndDiscardCheckedHandle. Abstracted interactions with EmailService and MimeHelper into sendOpenPasswordChangeEmail. Spotted timing attack vulnerability which enables user enumeration.

--- src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthChangePasswordService.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/auth/AuthChangePasswordService.kt
@@ -9,6 +9,7 @@ import com.yourRPG.chatPG.infra.uri.FrontendUriHelper
 import com.yourRPG.chatPG.security.requesthandle.RequestHandleService
 import com.yourRPG.chatPG.security.requesthandle.RequestHandleSubject
 import com.yourRPG.chatPG.service.account.AccountService
+import com.yourRPG.chatPG.validator.account.PasswordValidator
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -29,12 +30,14 @@ class AuthChangePasswordService(
     @param:Value("\${security.request-handle.change-password-expires-in}")
     private val changePasswordExpiresIn: Duration,
 
+    private val passwordValidator: PasswordValidator
 ) {
 
     companion object {
         private val subject = RequestHandleSubject.CHANGE_PASSWORD
     }
 
+    //FIXME: Vulnerable to timing attacks
     @Transactional
     fun openPasswordChange(dto: OpenPasswordChangeDto) {
         val account: Account =
@@ -43,25 +46,30 @@ class AuthChangePasswordService(
         val uuid = requestHandleService.newRequestHandle(account, subject)
 
         val url = frontendUriHelper.appendString("/login/forgot-password/$uuid")
-        val html = mimeHelper.getTemplate(
-            template  = "mime-change-password",
-            variables = arrayOf("url" to url)
-        )
 
+        sendOpenPasswordChangeEmail(dto.email, url)
+    }
+
+    fun sendOpenPasswordChangeEmail(email: String, url: String) {
         emailService.sendMimeEmail(
             subject = "Reset password",
-            to = dto.email,
-            html
+            to = email,
+            html = mimeHelper.getTemplate(
+                template  = "mime-change-password",
+                variables = arrayOf("url" to url)
+            )
         )
-
     }
 
     @Transactional
-    fun fulfillPasswordChange(dto: FulfillPasswordChangeDto): Unit = dto.run {
-        val uuidV7 = UUID.fromString(dto.requestHandle)
+    fun fulfillPasswordChange(dto: FulfillPasswordChangeDto) {
+        passwordValidator.validate(dto.password)
+
+        val requestHandle = UUID.fromString(dto.requestHandle)
 
         val account: Account =
-            requestHandleService.getAccountAndDiscardCheckedHandle(uuidV7,
+            requestHandleService.getAccountAndDiscardCheckedHandle(
+                requestHandle,
                 subject,
                 changePasswordExpiresIn
             )
