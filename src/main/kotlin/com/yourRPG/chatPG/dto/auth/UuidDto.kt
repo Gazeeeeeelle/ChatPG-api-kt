15d3c1c632a96ed283510a6f00455620b5901320
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:02:17 -0300

    FIXED: UuidDto.kt gave JSON Parsing exception when trying to deserialize the request, since there was no support to parse the UUID's representative string into the UUID object. The parsing is done in the 'value' field, which in object's instantiation parses the given uuid value, throwing IllegalArgumentException in case of parsing fails, which is then caught by GlobalExceptionHandler.kt as 400 Bad Request.

--- src/main/kotlin/com/yourRPG/chatPG/dto/auth/UuidDto.kt
+++ src/main/kotlin/com/yourRPG/chatPG/dto/auth/UuidDto.kt
@@ -1,9 +1,11 @@
 package com.yourRPG.chatPG.dto.auth
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import java.util.UUID
 
 data class UuidDto(
+    private val uuid: String,
 
-    val uuid: UUID
-
+    @field:JsonIgnore
+    val value: UUID = UUID.fromString(uuid)
 )
