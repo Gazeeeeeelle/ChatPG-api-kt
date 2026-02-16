f1080976199132a0bf15346a11062be91632856c
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:05:48 -0300

    Updated the AccessToChatFilter.kt's test according to its changes.

--- src/test/kotlin/com/yourRPG/chatPG/security/filters/AccessToChatFilterTest.kt
+++ src/test/kotlin/com/yourRPG/chatPG/security/filters/AccessToChatFilterTest.kt
@@ -1,6 +1,6 @@
 package com.yourRPG.chatPG.security.filters
 
-import com.yourRPG.chatPG.exception.chat.UnauthorizedAccessToChatException
+import com.yourRPG.chatPG.exception.chat.ForbiddenAccessToChatException
 import com.yourRPG.chatPG.security.helper.SecurityContextHelper
 import com.yourRPG.chatPG.validator.chat.AccountHasAccessToChatValidator
 import jakarta.servlet.http.HttpServletResponse
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.verify
+import java.util.UUID
 import java.util.stream.Stream
 
 class AccessToChatFilterTest: FilterTest() {
@@ -27,18 +28,20 @@ class AccessToChatFilterTest: FilterTest() {
     @Test
     fun `valid - path is filtered and attends to the requirements`() {
         //ARRANGE
+        val uuid = UUID.randomUUID()
+
         given(request.servletPath)
-            .willReturn("/chats/0/**")
+            .willReturn("/chats/by-public-id/$uuid/**")
 
         given(securityContext.getPrincipal())
             .willReturn(0L)
 
         //ACT
-        accessToChatFilter.doFilter(request, response, filterChain)
+        accessToChatFilter.doFilterInternal(request, response, filterChain)
 
         //ASSERT
         verify(validator)
-            .validate(0L to 0L)
+            .validate(0L to uuid)
 
         verify(filterChain)
             .doFilter(request, response)
@@ -56,7 +59,7 @@ class AccessToChatFilterTest: FilterTest() {
                     .willReturn(path)
 
                 //ACT
-                accessToChatFilter.doFilter(request, response, filterChain)
+                accessToChatFilter.doFilterInternal(request, response, filterChain)
 
                 //ASSERT
                 verify(filterChain)
@@ -69,17 +72,19 @@ class AccessToChatFilterTest: FilterTest() {
     @Test
     fun `invalid - did not attend to the validation`() {
         //ARRANGE
+        val uuid = UUID.randomUUID()
+
         given(request.servletPath)
-            .willReturn("/chats/0/**")
+            .willReturn("/chats/by-public-id/$uuid/**")
 
         given(securityContext.getPrincipal())
             .willReturn(0L)
 
-        given(validator.validate(0L to 0L))
-            .willThrow(UnauthorizedAccessToChatException::class.java)
+        given(validator.validate(0L to uuid))
+            .willThrow(ForbiddenAccessToChatException::class.java)
 
         //ACT
-        accessToChatFilter.doFilter(request, response, filterChain)
+        accessToChatFilter.doFilterInternal(request, response, filterChain)
 
         //ASSERT
         verify(response)
