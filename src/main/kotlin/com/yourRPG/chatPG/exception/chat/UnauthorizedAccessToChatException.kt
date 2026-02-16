db05c8f69b8532d19e85cda11c4f5bd1ea3b92b5
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 15:56:34 -0300

    Changed UnauthorizedAccessToChatException.kt into ForbiddenAccessToChatException.kt, which is more idiomatic for denied access on identified account.

--- src/main/kotlin/com/yourRPG/chatPG/exception/chat/UnauthorizedAccessToChatException.kt
+++
@@ -1,5 +0,0 @@
-package com.yourRPG.chatPG.exception.chat
-
-import com.yourRPG.chatPG.exception.http.UnauthorizedException
-
-class UnauthorizedAccessToChatException(message: String): UnauthorizedException(message)
