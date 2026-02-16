82c731994b1878b8f357ca000c4c52a377d69b43
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:12:21 -0300

    Added BASE_BY_PUBLIC_CHAT_ID, which uses the Chat's base path together with the identification method "by-public-id" followed by the Path Variable between braces. Changed indentation pattern for the path's String constants.

--- src/main/kotlin/com/yourRPG/chatPG/config/ApplicationEndpoints.kt
+++ src/main/kotlin/com/yourRPG/chatPG/config/ApplicationEndpoints.kt
@@ -5,13 +5,14 @@ object ApplicationEndpoints {
     object Auth {
         const val BASE = "/auth"
 
-        const val                    LOGIN = "/login"
-        const val              FULFILL_A2F = "/fulfill-a2f"
-        const val           REFRESH_TOKENS = "/refresh-tokens"
-        const val        LOGIN_WITH_HANDLE = "/login-with-handle"
-        const val     OPEN_PASSWORD_CHANGE = "/open-password-change"
-        const val  FULFILL_PASSWORD_CHANGE = "/fulfill-password-change"
-        const val    OPEN_ACCOUNT_CREATION = "/open-account-creation"
+        const val LOGIN = "/login"
+
+        const val FULFILL_A2F = "/fulfill-a2f"
+        const val REFRESH_TOKENS = "/refresh-tokens"
+        const val LOGIN_WITH_HANDLE = "/login-with-handle"
+        const val OPEN_PASSWORD_CHANGE = "/open-password-change"
+        const val FULFILL_PASSWORD_CHANGE = "/fulfill-password-change"
+        const val OPEN_ACCOUNT_CREATION = "/open-account-creation"
         const val FULFILL_ACCOUNT_CREATION = "/fulfill-account-creation"
     }
 
@@ -37,43 +38,46 @@ object ApplicationEndpoints {
     object AiModel {
         const val BASE = "/ai-models"
 
-        const val                ALL = "/all"
+        const val ALL = "/all"
         const val IS_MODEL_AVAILABLE = "/is-model-available"
     }
 
     object Chat {
         const val BASE = "/chats"
 
-        const val          ALL = "/all"
+        /**Path Variables: publicChatId*/
+        const val BASE_BY_PUBLIC_CHAT_ID = "$BASE/by-public-id/{publicChatId}"
+
+        const val ALL = "/all"
         /**Path Variables: chatName*/
-        const val      BY_NAME = "/by-name/{chatName}"
+        const val BY_NAME = "/by-name/{chatName}"
         const val CHOOSE_MODEL = "/choose-model"
         /**Path Variables: chatId*/
-        const val        MODEL = "/{chatId}/model"
+        const val MODEL = "/model"
     }
 
     object Message {
-        /**Path Variables: chatId*/
-        const val BASE = "${Chat.BASE}/{chatId}/messages"
+        /**Path Variables: publicChatId*/
+        const val BASE = "${Chat.BASE_BY_PUBLIC_CHAT_ID}/messages"
 
         /**Path Variables: referenceId*/
-        const val               NEW = "/new/{referenceId}"
+        const val NEW = "/new/{referenceId}"
         /**Path Variables: referenceId*/
-        const val               OLD = "/old/{referenceId}"
+        const val OLD = "/old/{referenceId}"
         /**Path Variables: messageId*/
-        const val       GET_MESSAGE = "/{messageId}"
+        const val GET_MESSAGE = "/{messageId}"
         const val GENERATE_RESPONSE = "/generate-response"
-        const val            DELETE = "/{messageId}"
+        const val DELETE = "/{messageId}"
         /**Path Variables: bound1, bound2*/
-        const val       BULK_DELETE = "/bulk-delete/{bound1}/{bound2}"
+        const val BULK_DELETE = "/bulk-delete/{bound1}/{bound2}"
     }
 
     object Poll {
-        const val BASE = "${Chat.BASE}/polls"
+        const val BASE = "${Chat.BASE_BY_PUBLIC_CHAT_ID}/polls"
 
         const val START = "/start"
-        const val  VOTE = "/vote"
-        const val   ALL = "/all"
+        const val VOTE = "/vote"
+        const val ALL = "/all"
     }
 
 }
