346a4b84543b726b89e97471eed34f19220d8820
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:21:44 -0300

    Updated queries to use Chat's public ID.

--- src/main/kotlin/com/yourRPG/chatPG/repository/MessageRepository.kt
+++ src/main/kotlin/com/yourRPG/chatPG/repository/MessageRepository.kt
@@ -6,12 +6,13 @@ import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import java.util.UUID
 
 interface MessageRepository: JpaRepository<Message, Long> {
 
     /**
      * Fetches all messages from the [chat].
-     * FIXME: minor scalability issue
+     * FIXME: minor scalability issue, needs pagination
      *
      * @param chat from where the messages will be fetched
      * @return [List] of [Message]s fetched
@@ -20,15 +21,15 @@ interface MessageRepository: JpaRepository<Message, Long> {
     fun qFindAllMessagesFromChat(chat: Chat): List<Message>
 
     /**
-     * Fetches 20 messages from the [Chat] identified with [chatId]. The messages are fetched by selecting the ids
+     * Fetches 20 messages from the [Chat] identified with [publicChatId]. The messages are fetched by selecting the ids
      *  less than the [reference] id, and therefore, 20 older messages than the reference.
      *
-     * @param chatId chat identifier
+     * @param publicChatId chat identifier
      * @param reference reference id for fetching
      * @return [List] of [Message]s fetched
      */
-    @Query("SELECT m FROM Message m WHERE m.chat.id = :chatId AND m.id < :reference GROUP BY m.id ORDER BY m.id DESC LIMIT 20")
-    fun qFindOldByChatIdAndReference(chatId: Long, reference: Long): List<Message>
+    @Query("SELECT m FROM Message m WHERE m.chat.publicId = :publicChatId AND m.id < :reference GROUP BY m.id ORDER BY m.id DESC LIMIT 20")
+    fun qFindOldByChatIdAndReference(publicChatId: UUID, reference: Long): List<Message>
 
     /**
      * Fetches 20 messages from the [Chat] identified with [chatId]. The messages are fetched by selecting the ids
@@ -38,18 +39,18 @@ interface MessageRepository: JpaRepository<Message, Long> {
      * @param reference reference id for fetching
      * @return [List] of [Message]s fetched
      */
-    @Query("SELECT m FROM Message m WHERE m.chat.id = :chatId AND m.id > :reference GROUP BY m.id ORDER BY m.id DESC LIMIT 20")
-    fun qFindNewByChatIdAndReference(chatId: Long, reference: Long): List<Message>
+    @Query("SELECT m FROM Message m WHERE m.chat.publicId = :publicChatId AND m.id > :reference GROUP BY m.id ORDER BY m.id DESC LIMIT 20")
+    fun qFindNewByChatIdAndReference(publicChatId: UUID, reference: Long): List<Message>
 
     /**
-     * Fetches one message from the [Chat] identified with [chatId], such that the message's id = [messageId].
+     * Fetches one message from the [Chat] identified with [publicChatId], such that the message's id = [messageId].
      *
-     * @param chatId chat identifier
+     * @param publicChatId chat identifier
      * @param messageId message identifier
      * @return [Message] found or else null
      */
-    @Query("SELECT m FROM Message m WHERE m.chat.id = :chatId AND m.id = :messageId")
-    fun findByChatIdAndId(chatId: Long, messageId: Long): Message?
+    @Query("SELECT m FROM Message m WHERE m.chat.publicId = :publicChatId AND m.id = :messageId")
+    fun findByPublicChatIdAndId(publicChatId: UUID, messageId: Long): Message?
 
     /**
      * Deletes a single message, with id [messageId], in the chat identified by [chatId].
@@ -59,9 +60,8 @@ interface MessageRepository: JpaRepository<Message, Long> {
      * @return Integer amount of messages deleted. One if successful, zero if no message were found for deletion.
      */
     @Modifying
-    @Transactional
-    @Query("DELETE FROM Message m WHERE m.chat.id = :chatId AND m.id = :messageId")
-    fun qDeleteByChatIdAndId(chatId: Long, messageId: Long): Int
+    @Query("DELETE FROM Message m WHERE m.chat.publicId = :publicChatId AND m.id = :messageId")
+    fun qDeleteByChatIdAndId(publicChatId: UUID, messageId: Long): Int
 
     /**
      * Deletes a multiple messages within a chat. The chat is identified by [chatId]. The range of messages deleted are
@@ -73,8 +73,7 @@ interface MessageRepository: JpaRepository<Message, Long> {
      * @return Integer amount of messages deleted. Bigger than zero if successful, zero if no message found for deletion.
      */
     @Modifying
-    @Transactional
-    @Query("DELETE FROM Message m WHERE m.chat.id = :chatId AND m.id BETWEEN :idStart AND :idFinish")
-    fun qBulkDeleteByChatIdFromIdToId(chatId: Long, idStart: Long, idFinish: Long): Int
+    @Query("DELETE FROM Message m WHERE m.chat.publicId = :publicChatId AND m.id BETWEEN :idStart AND :idFinish")
+    fun qBulkDeleteByChatIdFromIdToId(publicChatId: UUID, idStart: Long, idFinish: Long): Int
 
 }
