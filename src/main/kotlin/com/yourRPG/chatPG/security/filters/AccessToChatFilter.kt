bd2e10cd23a6cbead0a17d93cc78acc95ecc6864
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:05:09 -0300

    Updated AccessToChatFilter.kt to use the Chat's public ID.

--- src/main/kotlin/com/yourRPG/chatPG/security/filters/AccessToChatFilter.kt
+++ src/main/kotlin/com/yourRPG/chatPG/security/filters/AccessToChatFilter.kt
@@ -4,14 +4,13 @@ import com.yourRPG.chatPG.config.ApplicationEndpoints
 import com.yourRPG.chatPG.exception.http.HttpException
 import com.yourRPG.chatPG.security.helper.SecurityContextHelper
 import com.yourRPG.chatPG.validator.chat.AccountHasAccessToChatValidator
-import jakarta.servlet.Filter
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import java.util.UUID
 
 @Component
 class AccessToChatFilter(
@@ -19,24 +18,35 @@ class AccessToChatFilter(
     private val securityContext: SecurityContextHelper
 ): OncePerRequestFilter() {
 
-    override fun doFilterInternal(
+    private companion object {
+        val log = KotlinLogging.logger {}
+
+        const val GROUP_OF_UUID_CHARACTERS = "([-A-Fa-f0-9]+)"
+
+        val captureChatPublicIdRegex = ApplicationEndpoints.Chat.BASE_BY_PUBLIC_CHAT_ID
+            .replace(Regex(pattern = "\\{.*?}"), GROUP_OF_UUID_CHARACTERS)
+    }
+
+    public override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+        val regex = Regex(pattern = captureChatPublicIdRegex)
+
         val path = request.servletPath
-
-        val regex = Regex("${ApplicationEndpoints.Chat.BASE}/(\\d+)")
-
         val match: MatchResult? = regex.find(path)
 
         if (match != null) {
             try {
-                val chatId = match.groupValues[1].toLong()
+                val publicChatIdString = match.groupValues[1]
+                val publicChatId = UUID.fromString(publicChatIdString)
+
                 val accountId = securityContext.getPrincipal()
 
-                validator.validate(t = accountId to chatId)
+                validator.validate(t = accountId to publicChatId)
             } catch(ex: HttpException) {
+                log.warn { "Forbidden attempt to access chat" }
                 response.sendError(HttpServletResponse.SC_FORBIDDEN, ex.message)
                 return
             }
