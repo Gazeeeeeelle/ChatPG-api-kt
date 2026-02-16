cdba53ecc2e6dc55a5586836a980abd7c53491dc
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:16:10 -0300

    Added implementation of KLogger in the children classes of ExternalLoginController.kt, which allow for better identification of the service used for login.

--- src/main/kotlin/com/yourRPG/chatPG/controller/auth/external/GithubLoginController.kt
+++ src/main/kotlin/com/yourRPG/chatPG/controller/auth/external/GithubLoginController.kt
@@ -3,12 +3,15 @@ package com.yourRPG.chatPG.controller.auth.external
 import com.yourRPG.chatPG.config.ApplicationEndpoints
 import com.yourRPG.chatPG.infra.uri.FrontendUriHelper
 import com.yourRPG.chatPG.security.auth.external.GithubLoginService
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(ApplicationEndpoints.ExternalLogin.GITHUB)
 class GithubLoginController(
+    log: KLogger = KotlinLogging.logger {},
     githubLoginService: GithubLoginService,
     frontendUriHelper: FrontendUriHelper
-): ExternalLoginController(githubLoginService, frontendUriHelper)
+): ExternalLoginController(log, githubLoginService, frontendUriHelper)
