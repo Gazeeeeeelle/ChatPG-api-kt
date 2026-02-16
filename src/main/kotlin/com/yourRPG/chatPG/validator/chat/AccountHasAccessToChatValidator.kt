ea105bbb1ccc2713d60201a44cf2e56f461a62f3
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 16:07:34 -0300

    Update validation to access chat to reference chat with its public ID.

--- src/main/kotlin/com/yourRPG/chatPG/validator/chat/AccountHasAccessToChatValidator.kt
+++ src/main/kotlin/com/yourRPG/chatPG/validator/chat/AccountHasAccessToChatValidator.kt
@@ -2,17 +2,18 @@ package com.yourRPG.chatPG.validator.chat
 
 import com.yourRPG.chatPG.exception.account.AccountNotFoundException
 import com.yourRPG.chatPG.exception.chat.ChatNotFoundException
-import com.yourRPG.chatPG.exception.chat.UnauthorizedAccessToChatException
+import com.yourRPG.chatPG.exception.chat.ForbiddenAccessToChatException
 import com.yourRPG.chatPG.repository.AccountRepository
 import com.yourRPG.chatPG.repository.ChatRepository
 import com.yourRPG.chatPG.validator.IValidatable
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class AccountHasAccessToChatValidator(
     private val accountRepository: AccountRepository,
     private val chatRepository: ChatRepository
-): IValidatable<Pair<Long, Long>> {
+): IValidatable<Pair<Long, UUID>> {
 
     /**
      * Checks if the [Pair] (accountId, chatId) identifies an existing [com.yourRPG.chatPG.domain.account.Account], an existing
@@ -22,19 +23,19 @@ class AccountHasAccessToChatValidator(
      * @return [Pair] given.
      * @throws AccountNotFoundException
      * @throws ChatNotFoundException
-     * @throws UnauthorizedAccessToChatException
+     * @throws ForbiddenAccessToChatException
      */
-    override fun validate(t: Pair<Long, Long>) {
-        val (accountId, chatId) = t
+    override fun validate(t: Pair<Long, UUID>) {
+        val (accountId, publicChatId) = t
 
         if (!accountRepository.existsById(accountId))
             throw AccountNotFoundException("Account $accountId not found")
 
-        if (!chatRepository.existsById(chatId))
-            throw ChatNotFoundException("Chat $chatId not found")
+        if (!chatRepository.qExistsByPublicId(publicChatId))
+            throw ChatNotFoundException("Chat not found with public id given")
 
-        if (!chatRepository.qExistsByAccountNameAndId(accountId, chatId))
-            throw UnauthorizedAccessToChatException("Account $accountId cannot access chat $chatId")
+        if (!chatRepository.qExistsByAccountNameAndId(accountId, publicChatId))
+            throw ForbiddenAccessToChatException("Access denied")
 
     }
 
