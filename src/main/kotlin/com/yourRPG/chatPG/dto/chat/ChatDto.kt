b3fdb309c15ff8f82b4da391d41245566727a321
Daniel Sefstrom Pavanati <danielpavanati06@gmail.com>
2026-02-16 15:44:04 -0300

    Implemented use of public ID instead of sequential for Chat's DTO.

--- src/main/kotlin/com/yourRPG/chatPG/dto/chat/ChatDto.kt
+++ src/main/kotlin/com/yourRPG/chatPG/dto/chat/ChatDto.kt
@@ -2,11 +2,12 @@ package com.yourRPG.chatPG.dto.chat
 
 import jakarta.validation.constraints.NotBlank
 import org.jetbrains.annotations.NotNull
+import java.util.*
 
 data class ChatDto (
 
     @field:NotNull
-    val id: Long?,
+    val publicId: UUID?,
 
     @field:NotBlank
     val name: String?
